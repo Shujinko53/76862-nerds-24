a50066fe10039842c1ffc3aa4c28ffc62ded571d
Shujinko53 <46525278+Shujinko53@users.noreply.github.com>
2019-02-27 18:53:27 +0300

    скрипт и добавления в css и в html

--- js/script.js
+++ js/script.js
@@ -1,58 +1,96 @@
-var linkWriteUs = document.querySelector('.write-us-link');
 var modal = document.querySelector('.modal-write-us');
-var modalClose = modal.querySelector('.cross');
+var modalOpener = document.querySelector('.write-us-link');
+var modalCloser = modal.querySelector('.cross');
 var modalForm = modal.querySelector('.write-us-form');
-var login = modal.querySelector('.write-name');
-var email = modal.querySelector('.write-email');
-var commt = modal.querySelector('.write-comnt');
 
-var isStorageSupport = true;
-var storage = '';
+var nameField = modal.querySelector('.write-name');
+var emailField = modal.querySelector('.write-email');
+var letterField = modal.querySelector('.write-comnt');
+var submitButton = modal.querySelector('[type="submit"]');
 
-try {
-  storage = localStorage.getItem('login');
-} catch (err) {
-  isStorageSupport = false;
-}
-
-linkWriteUs.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  modal.classList.add('modal-show');
-  login.focus();
-  if (storage) {
-    login.value = storage;
-    email.focus();
-  } else {
-    login.focus();
-  }
-});
-
-modalClose.addEventListener('click', function (evt) {
+// Действия при закрытии модального окна
+var closeHandler = function (evt) {
   evt.preventDefault();
   modal.classList.remove('modal-show');
   modal.classList.remove('modal-error');
+  modalForm.classList.remove('modal-invalid');
+};
+
+var isStorageSupport = true;
+var storedName = '';
+
+try {
+  storedName = localStorage.getItem('name');
+} catch (error) {
+  isStorageSupport = false;
+}
+if (isStorageSupport) {
+  var storedEmail = localStorage.getItem('email');
+}
+
+// Временное отключение валидации для анимации невалидной формы
+modalForm.setAttribute('novalidate', true);
+
+modalOpener.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  modal.classList.add('modal-show');
+  nameField.focus();
+
+  // Заполнение данными из хранилища и фокус на первое пустое поле
+  if (storedName) {
+    nameField.value = storedName;
+    emailField.focus();
+  }
+  if (storedEmail) {
+    emailField.value = storedEmail;
+
+    if (nameField.value) {
+      letterField.focus();
+    }
+  }
 });
 
+modalCloser.addEventListener('click', closeHandler);
+
 modalForm.addEventListener('submit', function (evt) {
-  if (!name.value || !email.value || !commt.value) {
-    evt.preventDefault();
+  evt.preventDefault();
 
-    modal.classList.remove('modal-error');
-    modal.offsetWidth = modal.offsetWidth;
-    modal.classList.add('modal-error');
+  // Добавление класса, при котором невалидные поля подсвечены
+  // Это нужно, чтобы до первого сабмита форма не была с красными полями
+  modalForm.classList.add('modal-invalid');
 
-    /* setTimeout(function () {
+  // Включение валидации формы
+  modalForm.removeAttribute('novalidate');
+
+  if (!nameField.validity.valid || !emailField.validity.valid || !letterField.validity.valid) {
+      // Анимация невалидной формы
+      modal.classList.remove('modal-error');
+      modal.width = modal.offsetWidth;
       modal.classList.add('modal-error');
-    }, 0); */
+
+      setTimeout(function() {
+        // Повторный вызов сабмита при включенной валидации покажет подсказки
+        submitButton.click();
+
+        // Временное отключение валидации для анимации невалидной формы
+        modalForm.setAttribute('novalidate', true);
+      }, 0);
+  } else {
+    // Запись валидных данных в хранилище
+    if (isStorageSupport) {
+      localStorage.setItem('name', nameField.value);
+      localStorage.setItem('email', emailField.value);
+    }
+
+    // Отправка валидной формы
+    modalForm.submit();
   }
 });
 
 window.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
     if (modal.classList.contains('modal-show')) {
-      evt.preventDefault();
-      modal.classList.remove('modal-show');
-      modal.classList.remove('modal-error');
+      closeHandler(evt);
     }
   }
 });
